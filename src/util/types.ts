654a0b6697a7fc232d52e56922cb47494722bba1
KRISHNA K <kkrishna22572@gmail.com>
2023-02-04 19:49:51 +0530

    :boom: google sign-in:white_check_mark:

---
+++ src/util/types.ts
@@ -0,0 +1,45 @@
+import { ConversationPopulated } from "../../../backend/src/util/types";
+
+/*
+ * Users
+ */
+
+export interface CreateUsernameData {
+  createUsername: {
+    success: boolean;
+    error: string;
+  };
+}
+
+export interface CreateUsernameVariables {
+  username: string;
+}
+
+export interface SearchUsersInput {
+  username: string;
+}
+
+export interface SearchUsersData {
+  searchUsers: Array<SearchedUser>;
+}
+
+export interface SearchedUser {
+  id: string;
+  username: string;
+}
+
+/* Converation
+ */
+
+export interface ConverationData {
+  conversations: Array<ConversationPopulated>;
+}
+export interface CreateConversationData {
+  createConversation: {
+    conversationId: string;
+  };
+}
+
+export interface CreateConversationInput {
+  participantIds: Array<string>;
+}
