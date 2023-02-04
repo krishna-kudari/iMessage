1e659323929e2036c53c0022fa3e2ab30145f871
KRISHNA K <kkrishna22572@gmail.com>
2023-02-04 19:32:27 +0530

    :heavy_plus_sign: chakra-ui theme setup

---
+++ src/chakra/theme.ts
@@ -0,0 +1,23 @@
+import {extendTheme, ThemeConfig} from '@chakra-ui/react';
+
+const config: ThemeConfig = {
+    initialColorMode: 'dark',
+    useSystemColorMode: false,
+};
+
+const theme = extendTheme({config} ,{
+    colors: {
+        brand: {
+            100 : '#3d84f7',
+        },
+    },
+    styles: {
+        global: () => ({
+            body: {
+                bg: "blackAlpha.200",
+            }
+        })
+    }
+})
+
+export default theme;
