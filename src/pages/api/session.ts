a3c24048b6b81f73536c2aa89a3fa038e1dccdb8
KRISHNA K <kkrishna22572@gmail.com>
2023-02-10 00:34:36 +0530

    :safety_vest: next api session handler

---
+++ src/pages/api/session.ts
@@ -0,0 +1,18 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Session } from "next-auth";
+import { getSession } from "next-auth/react";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Session | null>
+) {
+  try {
+    const session = await getSession({ req });
+    console.log("SESSION FROM API ROUTE:⚡😍", session);
+    res.status(200).json(session);
+  } catch (error:any) {
+    console.log("GETSESSION ERROR:", error.message);
+    res.status(500);
+  }
+}
